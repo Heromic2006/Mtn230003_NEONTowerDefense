05469f82d30d7bb11b0072ded2f16dc71c57b8c4
Heromic2006 <heromic2006@gmail.com>
2026-04-20 10:06:37 -0500

    Added README file, requirments text file, and my base project.py

---
+++ src/project.py
@@ -0,0 +1,18 @@
+def main():
+    game = Game()
+    game.run()
+
+class Game:
+    def run(self):
+        pass
+
+class Tower:
+    def update(self):
+        pass
+
+class Enemy:
+    def move(self):
+        pass
+
+if __name__ == "__main__":
+    main()
